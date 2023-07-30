9f830c3a659b57a9271ca8dc7732a8895bba3c18
Shephali <shephalishukla1@gmail.com>
2023-07-30 22:54:37 +0530

    Added java code for array manipulation

---
+++ Data-Structures/LinearDS/1D-Array/Basic.java
@@ -0,0 +1,14 @@
+public class Basic {
+    public static void main(String[] args) {
+        
+        //Array Declaration
+        int[] num;
+
+        //Array Initialization: Some memory gets attached
+        num = new int[10];
+
+        //Declaration & Initialization
+        int[] lumber = {1,2,3,4,5};
+        int[] number = new int[5];
+    }
+}
